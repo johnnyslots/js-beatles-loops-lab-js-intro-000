d4fa3fcfdc974351cc80c04ec1c749aa6099585b
Yoni Slotwiner <yonislots@gmail.com>
2017-10-04 13:44:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(let i = 0; array.length < 4; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array;
+};
+
+function johnLennonFacts(facts){
+  var array = [];
+  while(facts.indexOf !== -1){
+    array.push(facts[0] + "!!!");
+    facts++;
+  }
+  return array;
+}
